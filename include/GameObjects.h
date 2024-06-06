c15976040ca8b9d4415f1e360be193c04727e728
tzachho@gmail.com <tzachho@gmail.com>
2024-06-06 15:33:27 +0300

    d

---
+++ include/GameObjects.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class GameObjects
+{
+public:
+	GameObjects();
+private:
+	sf::RectangleShape m_shape;
+};
---
+++ include/MovingObjects.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class MovingObjects
+{
+public:
+
+private:
+
+};
